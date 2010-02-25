733f40dc63303c1941d3df3376721bb7ad5d49c0
John Lynch <john@ubuntu.localdomain>
2010-02-25 14:20:06 -0800

    changed

---
+++ bash_aliases
@@ -0,0 +1,7 @@
+alias ll='ls -l'
+alias la='ls -A'
+alias l='ls -CF'
+alias rm='rm -i'
+alias cp='cp -i'
+alias sapt='sudo aptitude'
+alias gs='git status'
--- create_links.sh
+++ create_links.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-ln -s ~/.config/screenrc ~/.screenrc
-ln -s ~/.config/vim-rails ~/.vim
-ln -s ~/.config/vim-rails/vimrc ~/.vimrc
+ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
+ln -s ~/dotfiles/gemrc ~/.gemrc
+ln -s ~/dotfiles/screenrc ~/.screenrc
+ln -s ~/dotfiles/vim-rails ~/.vim
+ln -s ~/dotfiles/vim-rails/vimrc ~/.vimrc
 
