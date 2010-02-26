40c81d5e1830095b29fd9dd15486ecf5af9c062f
John Lynch <johnrlynch@gmail.com>
2010-02-25 16:15:07 -0800

    foo

--- create_links.sh
+++ create_links.sh
@@ -3,6 +3,7 @@
 ln -s ~/dotfiles/bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/gemrc ~/.gemrc
 ln -s ~/dotfiles/screenrc ~/.screenrc
+ln -s ~/dotfiles/irbrc ~/.irbrc
 ln -s ~/dotfiles/vim-rails ~/.vim
 ln -s ~/dotfiles/vim-rails/vimrc ~/.vimrc
 
